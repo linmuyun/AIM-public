40c6e5ca18fc1638d561e857d64067348f106cf1
KathyLin <13340246004@fudan.edu.cn>
2016-09-28 11:09:20 +0800

    bootmain.c

--- boot/bootmain.c
+++ boot/bootmain.c
@@ -22,10 +22,130 @@
 
 #include <sys/types.h>
 #include <aim/boot.h>
+#include <elf.h>
+#define ELF_MAGIC 0x464C457FU  // "\x7FELF" in little endian
 
-__noreturn
-void bootmain(void)
+#define SECTSIZE  512
+
+typedef elf32hdr_t elfhdr;
+typedef elf32_phdr_t proghdr;
+
+void readseg(uint8_t*, uint32_t, uint32_t);
+
+static inline void
+outb(uint16_t port, uint8_t data)
 {
-	while (1);
+  asm volatile("out %0,%1" : : "a" (data), "d" (port));
 }
+
+static inline void
+stosb(void *addr, int data, int cnt)
+{
+  asm volatile("cld; rep stosb" :
+               "=D" (addr), "=c" (cnt) :
+               "0" (addr), "1" (cnt), "a" (data) :
+               "memory", "cc");
+}
+
+static inline uint8_t
+inb(uint16_t port)
+{
+  uint8_t data;
+
+  asm volatile("in %1,%0" : "=a" (data) : "d" (port));
+  return data;
+}
+
+static inline void
+insl(int port, void *addr, int cnt)
+{
+  asm volatile("cld; rep insl" :
+               "=D" (addr), "=c" (cnt) :
+               "d" (port), "0" (addr), "1" (cnt) :
+               "memory", "cc");
+}
+
+
+void
+bootmain(void)
+{
+  elfhdr *elf;
+  proghdr *ph, *eph;
+  void (*entry)(void);
+  uint8_t* pa;
+
+  elf = (elfhdr*)0x10000;  // scratch space
+
+  // Read 1st page off disk
+  readseg((uint8_t*)elf, 4096, 0);
+
+  // Is this an ELF executable?
+  if(*(uint32_t *)elf->e_ident != ELF_MAGIC)
+    return;  // let bootasm.S handle error
+
+  // Load each program segment (ignores ph flags).
+  ph = (proghdr*)((uint8_t*)elf + elf->e_phoff);
+  eph = ph + elf->e_phnum;
+  for(; ph < eph; ph++){
+    pa = (uint8_t*)ph->p_paddr;
+    readseg(pa, ph->p_filesz, ph->p_offset);
+    if(ph->p_memsz > ph->p_filesz)
+      stosb(pa + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
+  }
+
+  // Call the entry point from the ELF header.
+  // Does not return!
+  entry = (void(*)(void))(elf->e_entry);
+  entry();
+}
+
+void
+waitdisk(void)
+{
+  // Wait for disk ready.
+  while((inb(0x1F7) & 0xC0) != 0x40)
+    ;
+}
+
+// Read a single sector at offset into dst.
+void
+readsect(void *dst, uint32_t offset)
+{
+  // Issue command.
+  waitdisk();
+  outb(0x1F2, 1);   // count = 1
+  outb(0x1F3, offset);
+  outb(0x1F4, offset >> 8);
+  outb(0x1F5, offset >> 16);
+  outb(0x1F6, (offset >> 24) | 0xE0);
+  outb(0x1F7, 0x20);  // cmd 0x20 - read sectors
+
+  // Read data.
+  waitdisk();
+  insl(0x1F0, dst, SECTSIZE/4);
+}
+
+// Read 'count' bytes at 'offset' from kernel into physical address 'pa'.
+// Might copy more than asked.
+void
+readseg(uint8_t* pa, uint32_t count, uint32_t offset)
+{
+  uint8_t* epa;
+
+  epa = pa + count;
+
+  // Round down to sector boundary.
+  pa -= offset % SECTSIZE;
+
+  // Translate from bytes to sectors; kernel starts at sector 1.
+  offset = (offset / SECTSIZE) + 1;
+
+  // If this is too slow, we could read lots of sectors at a time.
+  // We'd write more to memory than asked, but it doesn't matter --
+  // we load in increasing order.
+  for(; pa < epa; pa += SECTSIZE, offset++)
+    readsect(pa, offset);
+}
+
+
 
